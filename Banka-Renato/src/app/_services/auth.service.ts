c6408a1840a9c68f8c1f4e504d4e8f222a613319
Renato <rembareeg@gmail.com>
2019-06-05 18:45:26 +0200

    Changed base url to environment.ts

--- Banka-Renato/src/app/_services/auth.service.ts
+++ Banka-Renato/src/app/_services/auth.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'http://localhost:56188/api/auth/';
+  
 
 
 constructor(private http: HttpClient) {}
 
 login(model: any){
-  return this.http.post(this.baseUrl + 'login', model).pipe(
+  return this.http.post(environment.baseUrl + 'login', model).pipe(
     map((response: any) => {
       const user = response;
       if(user){
@@ -22,7 +23,7 @@ login(model: any){
 }
 
 register(model: any){
-  return this.http.post(this.baseUrl + 'register', model);
+  return this.http.post(environment.baseUrl + 'register', model);
 }
 
 }
