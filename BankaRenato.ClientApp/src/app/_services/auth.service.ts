6931c95f7d54fc064c41d9dfbbec475c31b42be9
Renato <rembareeg@gmail.com>
2019-06-06 15:35:29 +0200

    Added better token reading and saving in ClientApp

--- BankaRenato.ClientApp/src/app/_services/auth.service.ts
+++ BankaRenato.ClientApp/src/app/_services/auth.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
 
+jwtHelper = new JwtHelperService();
+decodedToken: any;
 
 constructor(private http: HttpClient) {}
 
@@ -18,12 +20,18 @@ login(model: any){
       const user = response;
       if(user){
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
       }
     }))  
 }
 
 register(model: any){
   return this.http.post(environment.baseUrl + 'register', model);
+}
+
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 }
--- README.md
+++ README.md
@@ -5,13 +5,12 @@ Added:
 Registration
 Login
 Simple token usage
+Better token reading ClientApp
 
 Soon:
-Better token reading
 Adding multiple types of users
 Adding Administrator / User UI
 Adding Cards
 ....
 
 
-Dont forget to change "appsettings.json" so you can connect do database.
